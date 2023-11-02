7ddca6c3250ba0fdacd81283c3cd95e7ff19e8db
Jefferson Santos <jeffdeutsch1@gmail.com>
2023-11-02 02:19:55 -0300

    removed unused inserts

--- pkg/sql/migrations/000001_init.up.sql
+++ pkg/sql/migrations/000001_init.up.sql
@@ -4,10 +4,3 @@ CREATE TABLE orders (
     tax FLOAT NOT NULL,
     final_price FLOAT NOT NULL
 )
-
-INSERT INTO orders (id, price, tax, final_price) VALUES ("aaa", 25.0, 0.5, 25.5)
-INSERT INTO orders (id, price, tax, final_price) VALUES ("bbb", 30.0, 0.5, 30.5)
-INSERT INTO orders (id, price, tax, final_price) VALUES ("ccc", 35.0, 0.5, 35.5)
-INSERT INTO orders (id, price, tax, final_price) VALUES ("ddd", 40.0, 0.5, 40.5)
-INSERT INTO orders (id, price, tax, final_price) VALUES ("eee", 15.0, 0.5, 15.5)
-INSERT INTO orders (id, price, tax, final_price) VALUES ("fff", 10.0, 0.5, 10.5)
